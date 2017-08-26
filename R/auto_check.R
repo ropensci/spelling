4b0f3418198740a32e8b9d0dc33f10a8b4ef33af
Jeroen <jeroenooms@gmail.com>
2017-08-26 23:19:57 +0200

    Updates

--- R/auto_check.R
+++ R/auto_check.R
@@ -2,14 +2,25 @@
 #'
 #' Setup automatic spell checking as a unit test for `CMD check`.
 #'
-#' The [spell_check_test()] function is intended to be called as a unit test triggered
-#' via running `CMD check` on a package. Use [spell_check_setup()] to automatically
-#' create the unit test that does this.
+#' Use [spell_check_setup()] to add a unit test to your package that that automatically
+#' spell checks documentation and vignettes when running `R CMD check`.
 #'
 #' @export
 #' @family spelling
-#' @rdname autocheck
+#' @aliases spell_check_test
+#' @rdname autotest
+#' @name autotest
 #' @inheritParams spell_check
+spell_check_setup <- function(path = ".", vignettes = TRUE, lang = "en_US"){
+  path <- normalizePath(path, mustWork = TRUE)
+  update_wordlist(path)
+  dir.create(file.path(path, "tests"), showWarnings = FALSE)
+  writeLines(sprintf("spelling::spell_check_test(vignettes = %s, lang = %s)",
+                     deparse(vignettes), deparse(lang)), file.path(path, "tests/spelling.R"))
+  cat(sprintf("Updated %s\n", file.path(path, "tests/spelling.R")))
+}
+
+#' @export
 spell_check_test <- function(vignettes = TRUE, lang = "en_US"){
   pkg_dir <- list.files("../00_pkg_src", full.names = TRUE)
   if(!length(pkg_dir)){
@@ -21,15 +32,4 @@ spell_check_test <- function(vignettes = TRUE, lang = "en_US"){
     stop(sprintf("Potential spelling errors: %s\nIf these are false positives, run spelling::update_wordlist()",
                  paste(names(results),collapse = ", ")))
   cat("All good.\n")
-}
-
-#' @export
-#' @rdname autocheck
-spell_check_setup <- function(path = ".", vignettes = TRUE, lang = "en_US"){
-  path <- normalizePath(path, mustWork = TRUE)
-  update_wordlist(path)
-  dir.create(file.path(path, "tests"), showWarnings = FALSE)
-  writeLines(sprintf("spelling::spell_check_test(vignettes = %s, lang = %s)",
-                     deparse(vignettes), deparse(lang)), file.path(path, "tests/spelling.R"))
-  cat(sprintf("Updated %s\n", file.path(path, "tests/spelling.R")))
 }
