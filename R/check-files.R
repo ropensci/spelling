c20a15e62c126c79f3d15be4b81b4fc3dde41188
Jeroen <jeroenooms@gmail.com>
2017-08-27 19:55:37 +0200

    Type check

--- R/check-files.R
+++ R/check-files.R
@@ -13,6 +13,7 @@
 #'   pattern = "\\.(Rnw|Rmd|html)$", full.names = TRUE)
 #' spell_check_files(files)
 spell_check_files <- function(path, ignore = character(), lang = "en_GB"){
+  stopifnot(is.character(ignore))
   dict <- hunspell::dictionary(lang, add_words = ignore)
   path <- normalizePath(path, mustWork = TRUE)
   lines <- lapply(sort(path), spell_check_file_one, dict = dict)
@@ -37,6 +38,7 @@ spell_check_file_one <- function(path, dict){
 #' @export
 #' @param text character vector with plain text
 spell_check_text <- function(text, ignore = character(), lang = "en_GB"){
+  stopifnot(is.character(ignore))
   dict <- hunspell::dictionary(lang, add_words = ignore)
   bad_words <- hunspell::hunspell(text, dict = dict)
   words <- sort(unique(unlist(bad_words)))
