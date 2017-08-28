80d2e7eb8bea0e28882e8aea36ea8be96558b92d
Jeroen <jeroenooms@gmail.com>
2017-08-28 13:55:55 +0200

    Support DESCRIPTION.in files

--- R/spell-check.R
+++ R/spell-check.R
@@ -69,7 +69,12 @@ spell_check_package <- function(pkg = ".", vignettes = TRUE, lang = "en_GB", use
 as_package <- function(pkg){
   if(inherits(pkg, 'package'))
     return(pkg)
-  description <- normalizePath(file.path(pkg, "DESCRIPTION"), mustWork = TRUE)
+  path <- pkg
+  description <- if(!file.exists(file.path(path, "DESCRIPTION")) && file.exists(file.path(path, "DESCRIPTION.in"))){
+    file.path(path, "DESCRIPTION.in")
+  } else {
+    normalizePath(file.path(path, "DESCRIPTION"), mustWork = TRUE)
+  }
   pkg <- as.list(read.dcf(description)[1,])
   names(pkg) <- tolower(names(pkg))
   pkg$path <- dirname(description)
