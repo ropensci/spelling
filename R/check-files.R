02363b7f2c5eb5a274bd5782499cbb039bd527b4
go_gonzo <dawid.kaledkowski@gmail.com>
2019-03-23 10:33:38 +0100

    adding .rd files to spell_check_files (#24)

--- R/check-files.R
+++ R/check-files.R
@@ -32,6 +32,8 @@ spell_check_files <- function(path, ignore = character(), lang = "en_US"){
 spell_check_file_one <- function(path, dict){
   if(grepl("\\.r?md$",path, ignore.case = TRUE))
     return(spell_check_file_md(path, dict = dict))
+  if(grepl("\\.rd$", path, ignore.case = TRUE))
+    return(spell_check_file_rd(path, dict = dict))
   if(grepl("\\.(rnw|snw)$",path, ignore.case = TRUE))
     return(spell_check_file_knitr(path = path, format = "latex", dict = dict))
   if(grepl("\\.(tex)$",path, ignore.case = TRUE))
